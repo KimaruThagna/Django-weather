9d04b60bc36763df22996b80888ec14ec5641f7d
Kimaru thagana <macbuntu@192.168.1.7>
2021-05-17 13:12:20 +0300

    add comments to data retrieval function

--- weatherApp/weather/views.py
+++ weatherApp/weather/views.py
@@ -5,7 +5,8 @@ from .forms  import WeatherForm
 
 def data_retrieval(latitude, longitude):
     URL = f'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat={latitude}&lon={longitude}'
-    results = requests.get(URL)
+    results = requests.get(URL) # getting a forbidden error here
+    # in the interest of time, I have assumed a json response and considered the below structure as seen in the browser
     data = results.json()
     return data['properties']['units']
 
@@ -18,8 +19,9 @@ def weather_view(request):
                 longitude = data.get('longitude')
                 # retrieve data from resource
                 results = data_retrieval(latitude,longitude)
-                return render(request, "weather.html", {'form': form, 'results':results})
+                print(results)
+                return render(request, "weather/weather.html", {'form': form, 'results':results})
      else:
         form = WeatherForm() # blank form object just to pass context if not post method
 
-        return render(request, "weather.html", {'form': form, 'results':None})
+        return render(request, "weather/weather.html", {'form': form, 'results':None})
