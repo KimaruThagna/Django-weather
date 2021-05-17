a0ad93e27080b9815d854a84922467043797c2f9
Kimaru thagana <macbuntu@192.168.1.7>
2021-05-17 12:17:06 +0300

    add HTML content for the form

--- weatherApp/weather/views.py
+++ weatherApp/weather/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from .forms  import WeatherForm,  
 
-# Create your views here.
 def weather_view(request):
-    
+     if request.method == 'POST':
+            form = WeatherForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                latitide = data.get('latitide')
+                longitude = data.get('longitude')
+                
+            
+                return HttpResponse("valid entiries")
+            else:
+                return HttpResponseRedirect('/')  # mention redirect url in argument
+     else:
+        form = WeatherForm() # blank form object just to pass context if not post method
+
+        return render(request, "weather.html", {'form': form})
