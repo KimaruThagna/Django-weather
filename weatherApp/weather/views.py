82ec621b82154a823db6ae7611a062a1d81d43a1
Kimaru thagana <macbuntu@192.168.1.7>
2021-05-17 12:44:40 +0300

    finish up data retrieval function

--- weatherApp/weather/views.py
+++ weatherApp/weather/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from .forms  import WeatherForm,  
 
+
+def data_retrieval(latitude, longitude):
+    URL = f'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat={latitude}&lon={longitude}'
+    
 def weather_view(request):
      if request.method == 'POST':
             form = WeatherForm(request.POST)
@@ -8,12 +12,10 @@ def weather_view(request):
                 data = form.cleaned_data
                 latitide = data.get('latitide')
                 longitude = data.get('longitude')
-                
-            
-                return HttpResponse("valid entiries")
-            else:
-                return HttpResponseRedirect('/')  # mention redirect url in argument
+                # retrieve data from resource
+                results = 1
+                return render(request, "weather.html", {'form': form, 'results':results})
      else:
         form = WeatherForm() # blank form object just to pass context if not post method
 
-        return render(request, "weather.html", {'form': form})
+        return render(request, "weather.html", {'form': form, 'results':None})
