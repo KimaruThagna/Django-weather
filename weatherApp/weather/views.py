1acb338183e59f9c480f3bb9628e37b59795c60f
Kimaru thagana <macbuntu@192.168.1.7>
2021-05-17 12:51:35 +0300

    clean up views

--- weatherApp/weather/views.py
+++ weatherApp/weather/views.py
@@ -1,19 +1,23 @@
+import requests
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
-from .forms  import WeatherForm,  
+from .forms  import WeatherForm
 
 
 def data_retrieval(latitude, longitude):
     URL = f'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat={latitude}&lon={longitude}'
-    
+    results = requests.get(URL)
+    data = results.json()
+    return data['properties']['units']
+
 def weather_view(request):
      if request.method == 'POST':
             form = WeatherForm(request.POST)
             if form.is_valid():
                 data = form.cleaned_data
-                latitide = data.get('latitide')
+                latitude = data.get('latitide')
                 longitude = data.get('longitude')
                 # retrieve data from resource
-                results = 1
+                results = data_retrieval(latitude,longitude)
                 return render(request, "weather.html", {'form': form, 'results':results})
      else:
         form = WeatherForm() # blank form object just to pass context if not post method
